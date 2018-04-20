d5b028213d3a1b6ac5523a6cf97bf606156c84ff
danyy <405507582@qq.com>
2018-04-21 00:56:18 +0800

    自动化创建junit测试类

--- src/main/java/com/danyy/seckill/dao/SuccessKilledDao.java
+++ src/main/java/com/danyy/seckill/dao/SuccessKilledDao.java
@@ -6,20 +6,21 @@ import org.apache.ibatis.annotations.Param;
 public interface SuccessKilledDao {
 
     /**
-     * 插入购买明细
+     * 插入购买明细，可过滤重复
+     *
      * @param seckillId
      * @param userPhone
-     * @return  插入的结果及数量(即行数)
+     * @return 插入的行数
      */
-    int insertSuccessKill(long seckillId, long userPhone);
+    int insertSuccessKilled(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
 
     /**
      * 根据id查询SuccessKilled并携带秒杀产品对象实体
      *
      * @param seckillId
+     * @param userPhone
      * @return
      */
-    SuccessKilled queryByIdWithSeckill(long seckillId);
-
+    SuccessKilled queryByIdWithSeckill(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
 
 }
---
+++ src/test/java/com/danyy/seckill/dao/SuccessKilledDaoTest.java
@@ -0,0 +1,41 @@
+package com.danyy.seckill.dao;
+
+import com.danyy.seckill.entity.SuccessKilled;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SuccessKilledDaoTest {
+
+    @Resource
+    private SuccessKilledDao successKilledDao;
+
+    @Test
+    public void insertSuccessKill() throws Exception {
+        /**
+         * 第一次:insertCount=1
+         * 第二次:insertCount=0
+         */
+        long id = 1001;
+        long phone = 13631231234L;
+        int insertCount = successKilledDao.insertSuccessKilled(id, phone);
+        System.out.println("insertCount=" + insertCount);
+    }
+
+    @Test
+    public void queryByIdWithSeckill() throws Exception {
+        long id = 1001;
+        long phone = 13631231234L;
+        SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(id, phone);
+        System.out.println(successKilled);
+        System.out.println(successKilled.getSeckill());
+    }
+
+}
